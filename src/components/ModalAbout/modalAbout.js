b53f63692ef15efb2f521eb39a0473f28f830269
OlexandrFilin <acfiln@ukr.net>
2024-02-25 22:52:25 +0200

    added close form Esc

--- src/components/ModalAbout/modalAbout.js
+++ src/components/ModalAbout/modalAbout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   BackdropModal,
   ContainerModal,
@@ -10,7 +10,19 @@ import AboutCar from './AboutCar/aboutCar';
 
 const ModalAbout = ({ carCard, closeLearnMore }) => {
   const { img, model } = carCard;
+  useEffect(() => {
+    window.addEventListener('keydown', handleCloseEscape);
+  });
+  useEffect(() => {
+    return () => {
+      console.log('222', 222)
+      window.removeEventListener('keydown', handleCloseEscape);
+    };
+  });
 
+  const handleCloseEscape = e => {
+    if (e.code === 'Escape') closeLearnMore();
+  };
   return (
     <BackdropModal>
       <ContainerModal>
